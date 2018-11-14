e8ce17e3bc6a74b2f2b48bbf49b11585c60e407a
Simon Hochholzer <simon@symmetronic.com>
2018-11-14 15:17:43 +0100

    setup methods for each plot

--- exercise2/src/index.js
+++ exercise2/src/index.js
@@ -9,17 +9,37 @@ const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
 /* Functions */
-function createPlot() {
+/* Creates a parallel coordinates plot. */
+function createParallelCoordinatesPlot() {
+	let svg = createSvg();
+
 	d3.csv('cars.csv', function(errors, cars) {
 		console.log(cars);
 		//x.domain(dimensions = d3.keys(cars[0].filter(function(dimension) {
 		//	return dimension != 'name' && y
 		//})
 	});
+
+	// TODO: Implement
 }
 
+/* Creates a scatter plot. */
+function createScatterPlot() {
+	let svg = createSvg();
+
+	// TODO: Implement
+}
+
+/* Creates a star plot. */
+function createStarPlot() {
+	let svg = createSvg();
+
+	// TODO: Implement
+}
+
+/* Creates an SVG element. */
 function createSvg() {
-	svg = d3.select('body').append('svg')
+	return d3.select('body').append('svg')
 			.attr('width', width + margin.left + margin.right)
 			.attr('height', height + margin.top + margin.bottom)
 		.append('g')
@@ -28,8 +48,9 @@ function createSvg() {
 
 /* Run program. */
 function main() {
-	createSvg();
-	createPlot();
+	createParallelCoordinatesPlot();
+	createScatterPlot();
+	createStarPlot();
 }
 
 window.onload = function() {
