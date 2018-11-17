e1af7bfe694db2a615bb6dc14e0b1511e8e16cc0
Simon Hochholzer <simon@symmetronic.com>
2018-11-17 16:06:46 +0100

    cleanup code

--- exercise2/src/index.js
+++ exercise2/src/index.js
@@ -1,81 +1,74 @@
-/* Constants */
-const margin = {
-	top: 30,
-	right: 10,
-	bottom: 10,
-	left: 10
-};
-const width = 960 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
-
 /* Functions */
 /**
  * Creates a parallel coordinates plot.
  */
 // SOURCE: Modified from https://bl.ocks.org/jasondavies/1341281
 function createParallelCoordinatesPlot(cars) {
-	console.log(cars);
+	console.log('create parallel coordinates plot');
+	
+	let svg = createSvg();
+	
 	// let x = d3.scaleOrdinal([0, width]);
 	// let y = {};
-	// let svg = createSvg();
 	// let axis = d3.svg.axis().orient('left');
 	// let dimensions;
 	// 
 	// /* X and y axes. */
 	// x.domain(dimensions = d3.keys(cars[0]).filter(function(dimension) {
-	// 	return dimension !== 'Name'
-	// 		&& (y[dimension] = d3.scaleLinear()
-	// 			.domain(d3.extent(cars, function(p) { return +p[d]; }))
-	// 			.range([height, 0]));
-	// }));
-
-	// /* Background lines. */
-	// let background = svg.append('g')
-	// 		.attr('class', 'background')
-	// 	.selectAll('path')
-	// 		.data(cars)
-	// 	.enter().append('path')
-	// 		.attr('d', path);
-
-	// /* Foreground lines. */
-	// let foreground = svg.append('g')
-	// 		.attr('class', 'foreground')
-	// 	.selectAll('path')
-	// 		.data('cars')
-	// 	.enter().append('path')
-	// 		.attr('d', path);
-
-	// /* Group element for each dimension. */
-	// let g = svg.selectAll('.dimension')
-	// 		.data(dimensions)
-	// 	.enter().append('g')
-	// 		.attr('class', 'dimension')
-	// 		.attr('transform', function(dimension) { return 'translate(' + x(dimension) + ')'; });
-	
-	// /* Add an axis and title. */
-	// g.append('g')
-	// 		.attr('class', 'axis')
-	// 		.each(function(dimension) { d3.select(this).call(axis.scale(y[dimension])); })
-	// 	.append('text')
-	// 		.style('text-anchor', 'middle')
-	// 		.attr('y', -9)
-	// 		.text(function(dimension) { return d; });
-
-	// // Returns the path for a given data point.
-	// function path(d) {
-	// 	return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
-	// }
-
-	// TODO: Implement
-}
-
-/**
- * Creates a scatter plot.
- */
-function createScatterPlot(cars) {
-	console.log(cars);
+		// 	return dimension !== 'Name'
+		// 		&& (y[dimension] = d3.scaleLinear()
+		// 			.domain(d3.extent(cars, function(p) { return +p[d]; }))
+		// 			.range([height, 0]));
+		// }));
 		
+		// /* Background lines. */
+		// let background = svg.append('g')
+		// 		.attr('class', 'background')
+		// 	.selectAll('path')
+		// 		.data(cars)
+		// 	.enter().append('path')
+		// 		.attr('d', path);
+		
+		// /* Foreground lines. */
+		// let foreground = svg.append('g')
+		// 		.attr('class', 'foreground')
+		// 	.selectAll('path')
+		// 		.data('cars')
+		// 	.enter().append('path')
+		// 		.attr('d', path);
+		
+		// /* Group element for each dimension. */
+		// let g = svg.selectAll('.dimension')
+		// 		.data(dimensions)
+		// 	.enter().append('g')
+		// 		.attr('class', 'dimension')
+		// 		.attr('transform', function(dimension) { return 'translate(' + x(dimension) + ')'; });
+		
+		// /* Add an axis and title. */
+		// g.append('g')
+		// 		.attr('class', 'axis')
+		// 		.each(function(dimension) { d3.select(this).call(axis.scale(y[dimension])); })
+		// 	.append('text')
+		// 		.style('text-anchor', 'middle')
+		// 		.attr('y', -9)
+		// 		.text(function(dimension) { return d; });
+		
+		// // Returns the path for a given data point.
+		// function path(d) {
+			// 	return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+			// }
+			
+			// TODO: Implement
+}
+		
+		/**
+		 * Creates a scatter plot.
+		 */
+function createScatterPlot(cars) {
+	console.log('create scatter plot');
+	
 	let svg = createSvg();
+	console.log(cars);
 	// TODO: Implement
 }
 
@@ -83,6 +76,8 @@ function createScatterPlot(cars) {
  * Creates a star plot.
  */
 function createStarPlot(cars) {
+	console.log('create star plot');
+	
 	let svg = createSvg();
 	// TODO: Implement
 }
@@ -91,11 +86,21 @@ function createStarPlot(cars) {
  * Creates an SVG element.
  */
 function createSvg() {
+	const margin = {
+		top: 30,
+		right: 10,
+		bottom: 10,
+		left: 10
+	};
+	const width = 960 - margin.left - margin.right;
+	const height = 500 - margin.top - margin.bottom;
+
 	return d3.select('body').append('svg')
 			.attr('width', width + margin.left + margin.right)
 			.attr('height', height + margin.top + margin.bottom)
 		.append('g')
-			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+			.attr('transform',
+			    'translate(' + margin.left + ',' + margin.top + ')');
 }
 
 /**
@@ -115,9 +120,7 @@ function getData() {
 	    return Promise.resolve(cars.map(function(car) {
 			return Object.keys(car).reduce(function(parsedCar, key) {
 				let value = car[key];
-				parsedCar[key] = isNumeric(value)
-					? parseFloat(value)
-					: value;
+				parsedCar[key] = isNumeric(value) ? parseFloat(value) : value;
 				return parsedCar;
 			}, {});
 	  }));
